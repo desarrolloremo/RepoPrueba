581969421ca30fe4d47e43edfdc842ee2a5b818f
desarrolloremo <git config --global user.email santiago@email.com>
2019-04-29 16:31:33 -0500

    hola mundo

---
+++ prueba1.php
@@ -0,0 +1,23 @@
+<?php
+
+// Ejercicio que utiliza variables numéricas y de cadena.
+
+// Conjuga ambos tipos de variables para ,finalmente, mostrar el resultado por pantalla.
+
+$cadena1="La suma de";
+
+$valor1=45;
+
+$cadena2=" y ";
+
+$valor2=22;
+
+$cadena3=" es:";
+
+$resultado=$valor1 + $valor2;
+
+$cadfinal=$cadena1.$valor1.$cadena2.$valor2.$cadena3.$resultado;
+
+echo $cadfinal;
+
+?>
